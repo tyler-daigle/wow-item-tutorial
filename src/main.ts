57f77247e640291d0cfab04147bff9f871deefb2
Tyler D <tyler@tdaigle.com>
2023-07-16 15:20:34 -0400

    setting the tooltip items attributes working

--- src/main.ts
+++ src/main.ts
@@ -19,10 +19,13 @@ function itemMouseOverHandler(e: MouseEvent) {
 
   const itemId = itemElement.getAttribute("data-itemid");
   const item = items.find((i) => i.id === itemId);
-  tooltipState.showTooltip(x, y);
   if (item) {
-    console.log(item);
+    tooltipState.setItem(item);
+    tooltipState.showTooltip(x, y);
   }
+  // if (item) {
+  //   console.log(item);
+  // }
 }
 
 function itemMouseOutHandler(e: MouseEvent) {
@@ -54,11 +57,14 @@ interface ToolTipState {
   tooltipVisible: boolean;
   hideTooltip: () => void;
   showTooltip: (x: number, y: number) => void;
+  setItem: (item: WowItem) => void;
   toolTipElement: HTMLDivElement;
   x: number;
   y: number;
 }
 
+//TODO: move all this into a class, have the constructor take a DOM
+// element to attach to or just generate the tooltip from a <template>
 const tooltipState: ToolTipState = {
   tooltipVisible: false,
   hideTooltip: function () {
@@ -72,6 +78,26 @@ const tooltipState: ToolTipState = {
     this.tooltipVisible = true;
     this.toolTipElement?.classList.remove("tooltip-hidden");
     this.toolTipElement?.classList.add("tooltip-visible");
+  },
+  setItem: function (item: WowItem) {
+    this.toolTipElement.querySelector(".tooltip-name")!.textContent = item.name;
+
+    const itemType = item.type.split(" ");
+    this.toolTipElement.querySelector(".tooltip-hands")!.textContent =
+      itemType[0];
+    this.toolTipElement.querySelector(".tooltip-weapon-type")!.textContent =
+      itemType[1];
+
+    const attributesList = document.createElement("ul");
+    Object.keys(item.attributes).forEach((attrib) => {
+      const li = document.createElement("li");
+      li.textContent = `+${item.attributes[attrib]} ${attrib}`;
+      attributesList.appendChild(li);
+    });
+    this.toolTipElement.querySelector(".tooltip-attributes")!.innerHTML = "";
+    this.toolTipElement
+      .querySelector(".tooltip-attributes")!
+      .appendChild(attributesList);
   },
   toolTipElement: document.querySelector("#tooltip")!,
   x: 10,
